c924bf0584f63fb07382c7a281285d42d959c39d
teto519f <tessa.todorowski@tu-dresden.de>
2025-06-05 10:55:33 +0200

    update exomat calls

--- tests/test_harness_full.sh
+++ tests/test_harness_full.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 #
-# integration test of all harness commands
+# integration test of all  commands
 #
 
 # get hwmondump binary from console input
@@ -25,44 +25,85 @@ trap cleanup EXIT SIGINT SIGTERM
 cd $DIR
 
 # missing subcommand
-! "$EXOMAT_BIN" harness
+! "$EXOMAT_BIN"
 
-# missing experiment
-! "$EXOMAT_BIN" harness skeleton
-! "$EXOMAT_BIN" harness env
-! "$EXOMAT_BIN" harness run
-# experiment is optional for harness make-table
+! "$EXOMAT_BIN" skeleton     # missing experiment name
+! "$EXOMAT_BIN" env          # not in an exmerpiment source directory
+! "$EXOMAT_BIN" run          # missing experiment name
+! "$EXOMAT_BIN" make-table   # not in an experiment series directory
+
+#
+# skeleton
+#
 
 # should work
-"$EXOMAT_BIN" harness skeleton One
+"$EXOMAT_BIN" skeleton One
 test -d $DIR/One
 test -f $DIR/One/template/run.sh
 test -f $DIR/One/envs/0.env
 
-# env needs the --add option for now
-! "$EXOMAT_BIN" harness env One
-test -f $DIR/One/envs/0.env         # not deleted
-test '!' -f $DIR/One/envs/1.env     # not created
+# already exists
+! "$EXOMAT_BIN" skeleton One
 
-# should work
-"$EXOMAT_BIN" harness env One --add ONE foo,bar
-test -f $DIR/One/envs/0.env
-test -f $DIR/One/envs/1.env
+#
+# env
+#
+
+# env needs to be called in an experiment source
+! "$EXOMAT_BIN" env One
+
+cd $DIR/One
+    # should work
+    "$EXOMAT_BIN" env
+
+    "$EXOMAT_BIN" env --add ONE foo,bar
+    test -f $DIR/One/envs/0.env
+    test -f $DIR/One/envs/1.env
+    test '!' -f $DIR/One/envs/2.env
+
+    "$EXOMAT_BIN" env --append ONE baz
+    test -f $DIR/One/envs/0.env
+    test -f $DIR/One/envs/1.env
+    test -f $DIR/One/envs/2.env
+
+    "$EXOMAT_BIN" env --remove ONE foo
+    test -f $DIR/One/envs/0.env
+    test -f $DIR/One/envs/1.env
+    test '!' -f $DIR/One/envs/2.env
+
+    "$EXOMAT_BIN" env --remove ONE
+    test -f $DIR/One/envs/0.env
+    test "$(cat $DIR/One/envs/0.env)" = ""
+    test '!' -f $DIR/One/envs/1.env
+    test '!' -f $DIR/One/envs/2.env
+
+    # setup for run
+    "$EXOMAT_BIN" env --add ONE foo,bar
+cd $DIR
+
+#
+# run
+#
 
 # experiment does not exist
 test '!' -d $DIR/non_existent
-! "$EXOMAT_BIN" harness run non_existent
+! "$EXOMAT_BIN" run non_existent
+
+# cannot be called in source
+cd $DIR/One
+    ! "$EXOMAT_BIN" run .
+cd $DIR
 
 # should work
-"$EXOMAT_BIN" harness run One
+"$EXOMAT_BIN" run One
 # from now on using -o, to access the output
 
 # should work
-"$EXOMAT_BIN" harness run One -o One_Out1
+"$EXOMAT_BIN" run One -o One_Out1
 test -d $DIR/One_Out1
-test -f $DIR/One_Out1/src/template/run.sh
-test -f $DIR/One_Out1/src/envs/0.env
-test -f $DIR/One_Out1/src/envs/1.env
+test -f $DIR/One_Out1/.src/template/run.sh
+test -f $DIR/One_Out1/.src/envs/0.env
+test -f $DIR/One_Out1/.src/envs/1.env
 test -f $DIR/One_Out1/runs/exomat.log
 test -f $DIR/One_Out1/runs/stderr.log
 test -f $DIR/One_Out1/runs/stdout.log
@@ -72,7 +113,7 @@ test -f $DIR/One_Out1/runs/run_1_rep0/run.sh
 test -f $DIR/One_Out1/runs/run_1_rep0/environment.env
 
 # only checking for correct number of repetitions
-"$EXOMAT_BIN" harness run One -o One_Out2 -r 3
+"$EXOMAT_BIN" run One -o One_Out2 -r 3
 test -d $DIR/One_Out2/runs/run_0_rep0/
 test -d $DIR/One_Out2/runs/run_1_rep0/
 test -d $DIR/One_Out2/runs/run_0_rep1/
@@ -83,22 +124,29 @@ test '!' -d $DIR/One_Out2/runs/run_0_rep3/
 test '!' -d $DIR/One_Out2/runs/run_1_rep3/
 
 # output already exists
-! "$EXOMAT_BIN" harness run One -o One_Out2
+! "$EXOMAT_BIN" run One -o One_Out2
 
-# should work
-"$EXOMAT_BIN" harness make-table One_Out1
-test -f $DIR/One_Out1/One_Out1.csv
-test "$(cat $DIR/One_Out1/One_Out1.csv)" = ""
+#
+# make-table
+#
 
-# create mock output
-echo 0 > $DIR/One_Out1/runs/run_0_rep0/out_foo
-echo a > $DIR/One_Out1/runs/run_0_rep0/out_bar
-echo 1 > $DIR/One_Out1/runs/run_1_rep0/out_foo
-echo b > $DIR/One_Out1/runs/run_1_rep0/out_bar
+# needs to be called in a series directory
+! "$EXOMAT_BIN" make-table One_Out1
 
-# should work
-"$EXOMAT_BIN" harness make-table One_Out1
-test -f $DIR/One_Out1/One_Out1.csv
-test "$(cat $DIR/One_Out1/One_Out1.csv)" = "foo,bar
-0,a
-1,b"
+cd $DIR/One_Out1
+    # should work
+    "$EXOMAT_BIN" make-table
+    test -f $DIR/One_Out1/One_Out1.csv
+    test "$(cat $DIR/One_Out1/One_Out1.csv)" != "" # contains envs
+
+    # create mock output
+    echo 0 > $DIR/One_Out1/runs/run_0_rep0/out_foo
+    echo a > $DIR/One_Out1/runs/run_0_rep0/out_bar
+    echo 1 > $DIR/One_Out1/runs/run_1_rep0/out_foo
+    echo b > $DIR/One_Out1/runs/run_1_rep0/out_bar
+
+    # should work
+    "$EXOMAT_BIN"  make-table
+    test -f $DIR/One_Out1/One_Out1.csv
+    test "$(cat $DIR/One_Out1/One_Out1.csv)" != "" # contains envs and output™
+cd $DIR
