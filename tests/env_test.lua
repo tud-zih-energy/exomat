861e3307fd5e6f47d3bb2cbc265638183550f22a
teto519f <tessa.todorowski@tu-dresden.de>
2026-02-04 08:08:52 +0100

    fix(lua env): union implementation

--- tests/env_test.lua
+++ tests/env_test.lua
@@ -2,10 +2,11 @@ freqs = from_list("FREQ", {1000, 2000, 3000})
 kernels = from_output("KERNELS", "add\nmul\ndiv")
 cpus = from_list("CPUS", {"0,1", "0,1,2,3"})
 
-result0 = freqs + freqs
+-- uncomment whichever you want to test, but only one at the time
+result = freqs + freqs
+-- result = cross({freqs, cpus, kernels})
+-- result = cross({freqs, cpus, kernels, from_list("TURBO", {"OFF"})}) + cross({from_list("FREQ", {3000}), cpus, kernels, from_list("TURBO", {"ON"})})
 
-result1 = cross({freqs, cpus, kernels})
-
-result2 = cross({freqs, cpus, kernels, from_list("TURBO", {"OFF"})}) + cross({from_list("FREQ", {3000}), cpus, kernels, from_list("TURBO", {"ON"})})
+return {result}
 
 
