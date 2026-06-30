289635be07682e1fc78af99c7eaa3619b2f656af
teto519f <tessa.todorowski@tu-dresden.de>
2026-06-30 08:52:39 +0200

    feat(summary): extend cli structure

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,11 @@ fn run_main(args: Cli, log_handler: indicatif::MultiProgress) -> Result<()> {
             remove,
         } => exomat::harness::env::main(add, append, remove),
         Commands::MakeTable { output } => exomat::harness::table::main(output),
+        Commands::Summary {
+            experiment,
+            estimate,
+            full,
+        } => exomat::harness::summary::main(experiment, estimate, full),
         Commands::Completion { shell } => bin::completion::main(shell),
     }
 }
